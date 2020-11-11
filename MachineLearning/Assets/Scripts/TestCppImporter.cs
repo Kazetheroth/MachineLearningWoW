2d85043a7d0f283e1c270a02bd66506a9378c606
Pim <pim103@hotmail.fr>
2020-11-11 14:21:08 +0100

    Init unity project - Add CppImporter and first version of dll

---
+++ MachineLearning/Assets/Scripts/TestCppImporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestCppImporter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(CppImporter.MyAdd(42.0, 51.0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var model = CppImporter.CreateLinearModel(4096);
+        var inputs = new double[] {1.0, 2.0, 3.0};
+        var result = CppImporter.PredictLinearModelMultiClassClassification(model,
+            inputs, 3, 3);
+        
+        var result_managed = new double[3];
+        
+        Marshal.Copy(result, result_managed, 0, 3);
+
+        foreach (var rslt in result_managed)
+        {
+            Debug.Log(rslt);
+        }
+        
+        CppImporter.DeleteLinearModel(result);
+        
+        CppImporter.DeleteLinearModel(model);
+    }
+}
